be6c9635653e64111b045b023af8fc3cfb266991
Beldaras <jphilippe.roblot@gmail.com>
2023-04-06 15:51:45 +0200

    update router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { authorization } = require("./middleware/auth");
+// const { authorization } = require("./middleware/auth");
 
 const mangaControllers = require("./controllers/mangaControllers");
 
