55344ce9bc6102ac45acedb56052488902527ba3
Beldaras <jphilippe.roblot@gmail.com>
2023-05-09 09:26:21 +0200

    add link to mail and open link in new tab

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import github from "../assets/icons/github-mark.svg";
 import linkedin from "../assets/icons/linkedin.svg";
+import mail from "../assets/icons/mail.svg";
 
 function Footer() {
   return (
     <div className="flex justify-center gap-4 bg-white opacity-70 pl-1 pr-1 w-full">
       <p className="text-sm">Roblot Jean-Philippe © - 2023</p>
-      <img src={github} alt="logo github" className="h-5 w-5" />
-      <img src={linkedin} alt="logo linkedin" className="h-5 w-5" />
+      <a href="https://github.com/Beldaras" target="_blank" rel="noreferrer">
+        <img src={github} alt="logo github" className="h-5 w-5" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/jean-philippe-roblot/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={linkedin} alt="logo linkedin" className="h-5 w-5" />
+      </a>
+      <a href="mailto:jphilippe.roblot@gmail.com">
+        <img src={mail} alt="logo email" className="h-5 w-5" />
+      </a>
     </div>
   );
 }
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import MenuBurger from "./MenuBurger";
-// import logo from "../assets/images/japan.png";
-
 
 function navBar() {
   return (
     <div className="w-full flex p-2 bg-slate-950/70">
       <MenuBurger />
       <h1 className="flex justify-center items-center ml-12 text-red text-xl font-semibold">Otaku’s Collection</h1>
-      {/*<img
-        src={logo}
-        alt="logo du site"
-        className="m-2 h-12 w-12"
-  />*/}
     </div>
   );
 }
